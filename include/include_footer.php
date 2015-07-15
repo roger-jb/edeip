6103b60fe9822a07bb9be1e36a90a8c5802af0cf
Jean-Baptiste <roger.jb.epsi@gmail.com>
2015-07-15 15:03:15 +0200

    ajout dossier 'include'

---
+++ include/include_footer.php
@@ -0,0 +1,8 @@
+<div class='footer' style="background">
+    <p>
+        <strong>EDEIP </strong><span class="droite">04 37 37 86 65</span><br/>
+	<span class="droite">06 62 35 85 84</span><br/>
+        27, rue Raoul Servant<br/>
+        69007 LYON<span class="droite"><a href="mailto:contact@edeip-lyon.fr" style='color:#65B1FF;'>contact@edeip-lyon.fr</a></span>
+    </p>
+</div>
