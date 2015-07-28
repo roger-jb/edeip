13742da9c166f0261b1a900724ee72d6c468fb68
Jean-Baptiste <roger.jeanbaptiste@gmail.com>
2015-07-29 00:18:50 +0200

    mise en place de la connexion à la BDD sur rPi_Dev, fonctionnel avec mysqli En attente de réponse de OVH concernant mysqli sur leur serveur.
    Prise en charge du réseau local ou distant pour la connexion à la bdd

--- ENV.php
+++ ENV.php
@@ -1,14 +1,13 @@
 <?php
 //en dev ==> localhost
 $__ENV = 'dev';
-$__DB_SRV = '192.168.10.4';
+
+if (substr($_SERVER['SERVER_ADDR'], 0, 3) == '192' || substr($_SERVER['SERVER_ADDR'], 0, 3) == '127') {
+    $__DB_SRV = '192.168.10.4';
+} else {
+    $__DB_SRV = 'roger-leoen.ddns.net';
+}
 $__DB_NAME = 'EDEIP';
 $__DB_USER = 'EDEIP';
-$__DB_MDP = 'EDEIP';
+$__DB_MDP = 'xpN7z7xX';
 
-
-//en test ==> rPi
-//$ENV = 'tests';
-
-//en prod ==> edeip-lyon.fr
-//$ENV = '';
